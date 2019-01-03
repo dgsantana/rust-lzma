32a81d9f434501e7cfe7b211487a096a9dc22310
Daniel Santana <danielgsantana@gmail.com>
2019-01-03 21:09:24 +0000

    Updated to use vcpkg.

--- build.rs
+++ build.rs
@@ -1,12 +1,5 @@
-extern crate pkg_config;
-
-use pkg_config::find_library;
-
+extern crate vcpkg;
 
 fn main() {
-	if find_library("liblzma").is_ok() {
-		return
-	} else {
-		panic!("Could not find liblzma using pkg-config")
-	}
+	if vcpkg::find_package("liblzma")..unwrap();
 }
