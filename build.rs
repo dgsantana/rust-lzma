9f8845facec1625cf2f30304381ab05190a7b72e
Daniel Santana <danielgsantana@gmail.com>
2019-02-18 11:18:15 +0000

    Added better support for linux

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
-extern crate vcpkg;
-
 fn main() {
-	vcpkg::find_package("liblzma").unwrap();
+	let target = std::env::var("TARGET").unwrap();
+	if target.contains("windows") {
+		vcpkg::find_package("liblzma").unwrap();
+	} else {
+		pkg_config::Config::new().probe("liblzma").unwrap();
+	}
 }
